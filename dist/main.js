23272cfa084e261d276361260f18fffd0b7ba85e
edrendon <edrendon@bancolombia.com.co>
2021-09-25 11:24:46 -0500

    Proyecto typeScript

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+console.log('Hola TypeScript');
+// Number
+// Explicito
+var phone;
+phone = 1;
+phone = 54234567;
+// phone = 'hola'; // Error
+// Inferido
+var phoneNumber = 54234567;
+phoneNumber = 123;
+// phoneNumber = true; // Error por tipo
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// Tipo: Boolean
+// Tipado Explicito
+var isPro;
+isPro = true;
+// isPro = 1;
+// Inferido
+var isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error!
+// Strings
+var username = 'luixaviles';
+username = "Luis";
+// username = true; // Error: tipo string
+// Template String
+// Uso de back-tick `
+var userInfo;
+userInfo = "\n    User Info:\n    username: " + username + "\n    firstName: " + (username + ' Aviles') + "\n    phone: " + phone + "\n    isPro: " + isPro + "\n";
+console.log('userInfo', userInfo);
---
+++ dist/type-any.js
@@ -0,0 +1,14 @@
+// Tipo explicito
+var idUser;
+idUser = 1; // number
+idUser = '1'; // string
+console.log('idUser', idUser);
+// Tipo Inferido
+var otherId;
+otherId = 1;
+otherId = '1';
+// otherId = true;
+console.log('otherId', otherId);
+var surprise = 'hello typescript';
+var res = surprise.substring(6);
+console.log('res', res);
